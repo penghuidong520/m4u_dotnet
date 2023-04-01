f91c6749fba73bddc7edc0907cc736d40690a7a6
penghuidong <penghuidong520@gmail.com>
2023-03-31 22:51:32 -0400

    update columns

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,13 @@ import './App.css';
 function App(): JSX.Element {
   return (
     <div className='app-container'>
-      
+        <div className="container">
+            <div className="widget">column 1</div>
+            <div className="widget">column 2</div>
+            <div className="widget">column 3</div>
+            <div className="widget">column 4</div>
+            <div className="widget">column 5</div>
+        </div>
     </div>
   );
 }
