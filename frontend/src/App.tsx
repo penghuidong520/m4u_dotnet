8c142416bfb007b8e6fa28c4c67e6e6db83627a3
penghuidong <penghuidong520@gmail.com>
2023-04-01 21:28:57 -0400

    update components need to update widgets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+import widgets from './components/widgets/widgets';
 
 function App(): JSX.Element {
   return (
     <div className='app-container'>
         <div className="container">
+            <Route path="/checkout" component={widgets}/>
             <div className="widget">column 1</div>
             <div className="widget">column 2</div>
             <div className="widget">column 3</div>
