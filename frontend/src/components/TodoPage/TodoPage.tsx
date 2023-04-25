67a60eefc4cbef31af9f3f4d65fd10ffaf33d946
penghuidong <penghuidong520@gmail.com>
2023-04-24 23:40:06 -0400

    update current date

--- frontend/src/components/TodoPage/TodoPage.tsx
+++ frontend/src/components/TodoPage/TodoPage.tsx
@@ -3,19 +3,22 @@ import React from 'react';
 function TodoList(): JSX.Element {
   const daysOfWeek: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const today: Date = new Date();
+  console.log(today);
   const currentDate: number = today.getDate();
+  console.log(currentDate)
   const currentDayOfWeek: string = daysOfWeek[today.getDay()];
+  console.log(currentDayOfWeek);
   const dates: number[] = Array.from({ length: 7 }, (_, i) => currentDate + i);
 
   return (
-    <div>
-      <h2>{currentDayOfWeek}</h2>
-      {dates.map((date: number) => (
-        <div key={date}>
-          <span>{daysOfWeek[(today.getDay() + dates.indexOf(date)) % 7]}</span>
-          <span>{date}</span>
-        </div>
-      ))}
+    <div className='todo-container'>
+        <div className='current-date'>currentDayOfWeek</div>
+        {dates.map((date: number) => (
+            <div key={date}>
+                <span>{daysOfWeek[(today.getDay() + dates.indexOf(date)) % 7]}</span>
+                <span>{date}</span>
+            </div>
+        ))}
     </div>
   );
 }
