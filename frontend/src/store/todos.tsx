98424c253124dd4235885e0987eb71d852e50a88
penghuidong <penghuidong520@gmail.com>
2023-04-27 08:52:00 -0400

    update edit todo reducer

--- frontend/src/store/todos.tsx
+++ frontend/src/store/todos.tsx
@@ -3,78 +3,113 @@ interface TodoItem {
 	id: number;
 	title: string;
 	description: string;
-}
-
-interface TodoListState {
+  }
+  
+  interface TodoListState {
 	[date: string]: TodoItem[];
-}
-
-// Define actions and their corresponding action creators
-enum TodoListActionTypes {
+  }
+  
+  // Define actions and their corresponding action creators
+  enum TodoListActionTypes {
 	ADD_TODO = 'ADD_TODO',
 	DELETE_TODO = 'DELETE_TODO',
-}
-
-interface AddTodoAction {
+	EDIT_TODO = 'EDIT_TODO',
+  }
+  
+  interface AddTodoAction {
 	type: TodoListActionTypes.ADD_TODO;
 	payload: {
-		date: string;
-		todoItem: TodoItem;
+	  date: string;
+	  todoItem: TodoItem;
 	};
-}
-
-interface DeleteTodoAction {
+  }
+  
+  interface DeleteTodoAction {
 	type: TodoListActionTypes.DELETE_TODO;
 	payload: {
-		date: string;
-		todoId: number;
+	  date: string;
+	  todoId: number;
 	};
-}
-
-function addTodoAction(date: string, todoItem: TodoItem): AddTodoAction {
+  }
+  
+  interface EditTodoAction {
+	type: TodoListActionTypes.EDIT_TODO;
+	payload: {
+	  date: string;
+	  todoId: number;
+	  updatedTodo: TodoItem;
+	};
+  }
+  
+  function addTodoAction(date: string, todoItem: TodoItem): AddTodoAction {
 	return {
-		type: TodoListActionTypes.ADD_TODO,
-		payload: {
-			date,
-			todoItem,
-		},
+	  type: TodoListActionTypes.ADD_TODO,
+	  payload: {
+		date,
+		todoItem,
+	  },
 	};
-}
-
-function deleteTodoAction(date: string, todoId: number): DeleteTodoAction {
+  }
+  
+  function deleteTodoAction(date: string, todoId: number): DeleteTodoAction {
 	return {
-		type: TodoListActionTypes.DELETE_TODO,
-		payload: {
-			date,
-			todoId,
-		},
+	  type: TodoListActionTypes.DELETE_TODO,
+	  payload: {
+		date,
+		todoId,
+	  },
 	};
-}
-
-// Define the reducer
-type TodoListAction = AddTodoAction | DeleteTodoAction;
-
-function todoListReducer(state: TodoListState = {}, action: TodoListAction): TodoListState {
+  }
+  
+  function editTodoAction(date: string, todoId: number, updatedTodo: TodoItem): EditTodoAction {
+	return {
+	  type: TodoListActionTypes.EDIT_TODO,
+	  payload: {
+		date,
+		todoId,
+		updatedTodo,
+	  },
+	};
+  }
+  
+  // Define the reducer
+  type TodoListAction = AddTodoAction | DeleteTodoAction | EditTodoAction;
+  
+  function todoListReducer(state: TodoListState = {}, action: TodoListAction): TodoListState {
 	switch (action.type) {
-		case TodoListActionTypes.ADD_TODO: {
-			const { date, todoItem } = action.payload;
-			const todoItems = state[date] ? [...state[date], todoItem] : [todoItem];
-			return {
-				...state,
-				[date]: todoItems,
-			};
-		}
-		case TodoListActionTypes.DELETE_TODO: {
-			const { date, todoId } = action.payload;
-			const todoItems = state[date].filter((item) => item.id !== todoId);
-			return {
-				...state,
-				[date]: todoItems,
-			};
-		}
-		default:
+	  case TodoListActionTypes.ADD_TODO: {
+		const { date, todoItem } = action.payload;
+		const todoItems = state[date] ? [...state[date], todoItem] : [todoItem];
+		return {
+		  ...state,
+		  [date]: todoItems,
+		};
+	  }
+	  case TodoListActionTypes.DELETE_TODO: {
+		const { date, todoId } = action.payload;
+		const todoItems = state[date].filter((item) => item.id !== todoId);
+		return {
+		  ...state,
+		  [date]: todoItems,
+		};
+	  }
+	  case TodoListActionTypes.EDIT_TODO: {
+		const { date, todoId, updatedTodo } = action.payload;
+		const todoItems = state[date].map((item) => {
+		  if (item.id === todoId) {
+			return updatedTodo;
+		  }
+		  return item;
+		});
+		return {
+		  ...state,
+		  [date]: todoItems,
+		};
+	  }
+	  default:
 		return state;
 	}
-}
-
-export default todoListReducer;
+  }
+  
+  export default todoListReducer;
+  
