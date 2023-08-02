e95c0e698cb76a3335f9f750a762c0faa0a0258b
penghuidong <penghuidong520@gmail.com>
2023-08-01 22:49:14 -0400

    update todo-item time css

--- frontend/src/components/TodoPage/TodoList.tsx
+++ frontend/src/components/TodoPage/TodoList.tsx
@@ -6,7 +6,10 @@ function TodoList({todos}: { todos: TodoItem[] }): JSX.Element {
         <ul className='todo-items-container'>
             {todos.map((todo: TodoItem)=> (
                 <li key={todo.id} className='todo-item'>
-                    <h3 className='todo-title' >{todo.title}</h3>
+                    <div className='todo-item-heading'>
+                        <h3 className='todo-title' >{todo.title}</h3>
+                        <span className='todo-time' >{todo.time}</span>
+                    </div>
                     <p className='todo-description'>{todo.description}</p>
                 </li>
             ))}
